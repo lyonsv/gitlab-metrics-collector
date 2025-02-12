e75b3b546f9707fec332fceed59df45f848cdfd3
Vincent Lyons <vincent@built.ie>
2025-02-12 17:52:02 +0000

    Fix linting errors.

--- src/exporters/html.js
+++ src/exporters/html.js
@@ -1,8 +1,4 @@
 import fs from 'fs/promises';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const HTML_TEMPLATE = `<!DOCTYPE html>
 <html lang="en">
